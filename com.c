d02464d780aad208c911b0ca0981e3714c266773
vanvincent <han077@usc.edu>
2014-04-28 18:11:03 +0800

    change for resetting string counter

--- com.c
+++ com.c
@@ -40,6 +40,8 @@ char* com_requestdata(char DeviceDATAID){
 			}else{
 				sprintf(buffer,"TIME_OUT\r");
 				count=0;
+				i=0;                //Reset buffer index
+				interruptstate=0;
 				return buffer
 			}
 			
