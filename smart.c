a61cb42055e661979f4ea51fbae4612956c29f97
vanvincent <han077@usc.edu>
2014-05-01 17:37:40 +0800

    add stuffs in smart.c

--- smart.c
+++ smart.c
@@ -1,12 +1,14 @@
 #include <cmath.h>
+#include "groom/rtc.h"
+
 
 #define Target_temp 22
 #define Target_brightness 75
 
 
-uint8_t Blind=
-
-
+uint8_t Blind_status     // 1 up   0 down
+uint8_t AC_status      // 1 heat on 2 heat off  3 cool on 4 cool off
+uint8_t Light_status // 2 full 1 half 0 off
 
 int temp_parse(char * temp){
 
@@ -31,17 +33,25 @@ int pd_parse(char* pd){
 	return atoi(*pd);
 }
 
-int day_parse(char *RTC){
+int day_parse(){
+	struct rtc_time t;
+	uint8_t res = rtc_get_time(&t);
+	if (res) {
+		return 1;
+	}
 
+	if(t.hours>6 && t.hours<19){
+		return 1;
+	}else{
+		return 0;
+	}
 }
 
 void smart_control(int temp, int pd, uint8_t day_night, int motion){
 	
-		
 
 
 
 
 }
 
-void 
