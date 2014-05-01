3446930c9c8b1dd3c8e78a398372df85b431fa58
vanvincent <han077@usc.edu>
2014-05-01 11:50:25 +0800

    Please read the comments in groom_master

--- groom_master.c
+++ groom_master.c
@@ -21,6 +21,44 @@
 #define MANUAL_TEMP 1
 #define MANUAL_LIGHT 2
 
+
+/*
+	Everyone working on the groom_master
+
+	Please use the #define in com.h to send or receive any command
+
+	a table is included here
+
+	//!! This protocol should be used in all uP.
+#define HB_ALPHA '1'
+#define HB_BETA '2'
+#define READ_ALPHA '3'
+#define READ_BETA '4'
+#define SEND_ALPHA '5'
+#define SEND_BETA '6'
+#define ACK 'A'
+#define ACTIVE_RESPONSE 'R'
+#define ACTIVE_RESPONSE_MOTION 'r'
+#define DEFAULT '0' 
+#define COMMAND_MODE 0
+#define TRANSMIT_MODE 1
+
+// For commands to BETA
+#define HEAT_ON     'H'
+#define HEAT_OFF    'h'
+#define COOL_ON     'C'
+#define COOL_OFF    'c'
+#define FAN_ON      'F'
+#define FAN_OFF     'f'
+#define BLINDS_UP   'U'
+#define BLINDS_DOWN 'D'
+#define LIGHTS_FULL 'L'
+#define LIGHTS_HALF 'i'
+#define LIGHTS_OFF  'l'
+
+
+*/
+
 struct status_item {
 	char *label;
 	char *format_string;
