cea99c0a285d7fd213987df3b0d997a939775813
jenrosemurphy <jenrosemurphy@gmail.com>
2014-05-02 10:54:47 -0700

    updated temperature checks

--- smart.c
+++ smart.c
@@ -58,52 +58,41 @@ int day_parse(){
 //uint8_t Light_status; // 2 full 1 half 0 off
 //uint8_t AC_status;      // 1 heat on 2 heat off  3 cool on 4 cool off
 void smart_control(int temp, int pd, uint8_t day_night, int motion){
+	if ((Target_temp+3)>temp) {	//if too cold, turn on heat
+		com_senddata(SEND_BETA, COOL_OFF);
+		com_senddata(SEND_BETA, FAN_ON);
+		com_senddata(SEND_BETA, HEAT_ON);
+		AC_status=1;	//update to say heat on
+	}
+	if ((Target_temp-3)<temp) { //if too hot, turn on air
+		com_senddata(SEND_BETA, HEAT_OFF);
+		com_senddata(SEND_BETA, FAN_ON);
+		com_senddata(SEND_BETA, COOL_ON);
+		AC_status=3;	//update to say air on
+	}	
 	if (day_night) {	//if it is daytime
-		if (Target_temp>temp) {	//if too cold, turn on heat
-			com_senddata('6', 'c');
-			com_senddata('6', 'F');
-			com_senddata('6', 'H');
-			AC_status=1;	//update to say heat on
-		}
-		if (Target_temp<temp) { //if too hot, turn on air
-			com_senddata('6', 'h');
-			com_senddata('6', 'F');
-			com_senddata('6', 'C');
-			AC_status=3;	//update to say air on
-		}	
+
 		if (~Blind_status) {		//if blinds down, put up
-			com_senddata('6', 'U');	
+			com_senddata(SEND_BETA, BLINDS_UP);	
 			Blind_status=1;
 		}		
 		if (Light_status!=2) {	//if lights not full, turn lights on
-			com_senddata('6', 'L');
+			com_senddata(SEND_BETA, LIGHTS_FULL);
 			Light_status=2;
 		}	
 		
 	}
 	if (~day_night) {	//if it is nighttime
-		if (Target_temp>temp) {	//if too cold, turn on heat
-			com_senddata('6', 'c');
-			com_senddata('6', 'F');
-			com_senddata('6', 'H');
-			AC_status=1;	//update to say heat on
-		}
-		if (Target_temp<temp) { //if too hot, turn on air
-			com_senddata('6', 'h');
-			com_senddata('6', 'F');
-			com_senddata('6', 'C');
-			AC_status=3;	//update to say air on
-		}	
 		if (Blind_status) {		//if blinds up, put down
-			com_senddata('6', 'D');	
+			com_senddata(SEND_BETA, BLINDS_DOWN);	
 			Blind_status=0;
 		}		
 		if (~Light_status&& motion) {	//if lights off & motion detected
-			com_senddata('6', 'i');
+			com_senddata(SEND_BETA, LIGHTS_HALF);
 			Light_status=1; //update to say lights half on
 		}	
 		if (Light_status!=0 && ~motion) {  //if lights on and no motion
-			com_senddata('6','l');
+			com_senddata(SEND_BETA,LIGHTS_OFF);
 			Light_status=0; //update to say lights off
 		}	
 	}	
